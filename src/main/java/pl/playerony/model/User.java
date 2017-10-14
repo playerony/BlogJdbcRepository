894686c39479b1a100f0d5fc73ec75371d5b08ad
Paweł <pawel.wojtasinski.1995@gmail.com>
2017-10-14 17:42:36 +0200

    Create User model

---
+++ src/main/java/pl/playerony/model/User.java
@@ -0,0 +1,72 @@
+package pl.playerony.model;
+
+public class User {
+	private Long id;
+	private String login;
+	private String password;
+	private Integer roleId;
+	
+	public User(Long id, String login, String password, Integer roleId) {
+		super();
+		this.id = id;
+		this.login = login;
+		this.password = password;
+		this.roleId = roleId;
+	}
+	
+	public User(String login, String password, Integer roleId) {
+		super();
+		this.login = login;
+		this.password = password;
+		this.roleId = roleId;
+	}
+	
+	/**
+	 * 
+	 * Getters
+	 * 
+	 * @return
+	 * 
+	 */
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public Integer getRoleId() {
+		return roleId;
+	}
+	
+	/**
+	 * 
+	 * Setters
+	 * 
+	 * @return
+	 * 
+	 */
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setRoleId(Integer roleId) {
+		this.roleId = roleId;
+	}
+	
+}
