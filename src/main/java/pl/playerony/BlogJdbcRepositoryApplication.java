f38a18a5c81300cbfe6edd9ab70565a03ffc2070
Paweł <pawel.wojtasinski.1995@gmail.com>
2017-10-14 16:59:39 +0200

    Initial commit

---
+++ src/main/java/pl/playerony/BlogJdbcRepositoryApplication.java
@@ -0,0 +1,12 @@
+package pl.playerony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogJdbcRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogJdbcRepositoryApplication.class, args);
+	}
+}
